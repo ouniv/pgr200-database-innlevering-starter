5d165f9b343326bee1073f9b3b28de185649fb17
Olav Bjørnli <olav_sp@hotmail.com>
2018-10-24 15:26:19 +0200

    koble mot database

--- src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
+++ src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
@@ -1,6 +1,23 @@
 package no.kristiania.pgr200.database;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseMain {
 
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=1234Lolz");
+            Statement statement = conn.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS brukere " +
+                                "(name TEXT, phone INTEGER, email TEXT)");
+
+        } catch(SQLException e) {
+            System.out.println("Noe gikk galt " + e.getMessage());
+
+        }
+    }
 }
